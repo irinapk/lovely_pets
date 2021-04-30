439d8312fcb67a9fabbb38f1c38b4ba6b08d01ab
Irina Park <park_irina@naver.com>
2021-04-30 22:18:42 +0900

    Initial commit

---
+++ src/main/java/demoproject/lovely_pets/LovelyPetsApplication.java
@@ -0,0 +1,13 @@
+package demoproject.lovely_pets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovelyPetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovelyPetsApplication.class, args);
+    }
+
+}
