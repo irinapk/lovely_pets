ed1c1d963bf168939314cddd7bf03e7011c6e629
Elena <aeglena@mail.ru>
2021-05-01 17:29:26 +0900

    Model Pet, User, Role, Post

--- src/main/java/demoproject/lovely_pets/back/model/User.java
+++ src/main/java/demoproject/lovely_pets/back/model/User.java
@@ -1,4 +1,38 @@
 package demoproject.lovely_pets.back.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    @ManyToMany(cascade = {CascadeType.PERSIST,
+            CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinTable(name = "role_users", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @JsonManagedReference
+    private Set<Role> roles;
+    @ManyToMany(cascade = {CascadeType.PERSIST,
+            CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinTable(name = "post_users", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "post_id"))
+    @JsonManagedReference
+    private Set <Post> posts;
+    @ManyToMany(cascade = {CascadeType.PERSIST,
+            CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinTable(name = "pet_users", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "pet_id"))
+    @JsonManagedReference
+    private Set <Pet> pets;
 }
